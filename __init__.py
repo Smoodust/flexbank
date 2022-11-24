d7b984803925d72c05abbbd4ced58f13335883f1
Smoodust <rustyburing@yandex.ru>
2022-11-24 23:28:37 +0300

    bot pizdec

---
+++ __init__.py
@@ -0,0 +1,109 @@
+import telebot as tb
+from enum import Enum
+from telebot import types
+import backend
+
+# class syntax
+class State(Enum):
+    authorize = 1
+    main_menu = 2
+    accounts = 3
+    ops = 4
+    concrete_card = 5
+
+bot = tb.TeleBot("5832977748:AAH0WbooWs5awOwb0ZfegCaH4j_zil4paBo")
+
+global states, connection
+states = {}
+connection = backend.get_connection()
+
+def print_authorize(message):
+    markup = types.ReplyKeyboardMarkup(row_width=4).add(types.KeyboardButton('Гончарова Мария'), types.KeyboardButton('Кондратьев Семен'))
+    bot.send_message(message.chat.id, "Хот ты?", reply_markup=markup)
+
+def print_main_menu(message):
+    card_reply = types.KeyboardButton('Счета')
+    ops_reply = types.KeyboardButton('Операции')
+
+    markup = types.ReplyKeyboardMarkup(row_width=4).add(card_reply, ops_reply)
+    print(states)
+    bot.send_message(message.chat.id, "Здраствуйте {} {}! Чем могу вам быть полезен?".format(states[message.from_user.id]['user_info']['surname'], states[message.from_user.id]['user_info']['name']), reply_markup=markup)
+
+def print_account(message):
+    accounts = states[message.from_user.id]['accounts']
+    accounts = ['{} {} {}'.format(x['number'], x['status'], x['type']) for x in accounts]
+    accounts = [str(i+1)+'. '+x for i, x in enumerate(accounts)]
+    markup = types.ReplyKeyboardMarkup(row_width=4).add(*[str(x+1) for x in range(len(accounts))], types.KeyboardButton('Назад'))
+    bot.send_message(message.chat.id, '\n'.join(accounts), reply_markup=markup)
+
+def print_concrete_account(message):
+    id = message.from_user.id
+    markup = types.ReplyKeyboardMarkup(row_width=4).add(types.KeyboardButton('Назад'))
+    bot.send_message(message.chat.id, str(states[id]['accounts'][states[id]['index']]), reply_markup=markup)
+
+def print_ops(message):
+    back_reply = types.KeyboardButton('Назад')
+
+    markup = types.ReplyKeyboardMarkup(row_width=4).add(back_reply)
+    bot.send_message(message.chat.id, "Операции", reply_markup=markup)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    id = message.from_user.id
+    states[id] = {'state':State.authorize}
+    print_authorize(message)
+
+@bot.message_handler(content_types=['text'])
+def handle_messages(message):
+    id = message.from_user.id
+    if id not in states:
+        states[id] = {'state':State.authorize}
+    
+    if states[id]['state'] == State.authorize:
+        if message.text == 'Гончарова Мария':
+            states[id] = {'state':State.main_menu, 'login':'QQKGJFPVFY', 'pass':'FociSsQIb8'}
+            states[id]['user_info'] = backend.get_user_by_login_pass(connection, states[id]['login'], states[id]['pass'])
+            states[id]['accounts'] = backend.get_active_accounts_by_user(connection, states[id]['user_info']['id'])
+            print_main_menu(message)
+        elif message.text == 'Кондратьев Семен':
+            states[id] = {'state':State.main_menu, 'login':'WQLXCYMEUZ', 'pass':'BGdcN1G72e'}
+            states[id]['user_info'] = backend.get_user_by_login_pass(connection, states[id]['login'], states[id]['pass'])
+            states[id]['accounts'] = backend.get_active_accounts_by_user(connection, states[id]['user_info']['id'])
+            print_main_menu(message)
+        else:
+            print_authorize(message)
+    elif states[id]['state'] == State.main_menu:
+        if message.text == 'Счета':
+            states[id]['state'] = State.accounts
+            print_account(message)
+        elif message.text == 'Операции':
+            states[id]['state'] = {'state':State.ops}
+            print_ops(message)
+        else:
+            print_main_menu(message)
+    elif states[id]['state'] == State.accounts:
+        if message.text == 'Назад':
+            states[id]['state'] = State.main_menu
+            print_main_menu(message)
+        elif message.text.isnumeric() and int(message.text) > 0 and int(message.text) <= len(states[id]['accounts']):
+            states[id]['state'] = State.concrete_card
+            states[id]['index'] = int(message.text) - 1
+            print_concrete_account(message)
+        else:
+            print_account(message)
+    elif states[id]['state'] == State.ops:
+        if message.text == 'Назад':
+            states[id]['state'] = State.main_menu
+            print_main_menu(message)
+        else:
+            print_ops(message)
+    elif states[id]['state'] == State.concrete_card:
+        if message.text == 'Назад':
+            states[id]['state'] = State.accounts
+            print_account(message)
+        else:
+            print_concrete_account(message)
+
+if __name__ == '__main__':
+    print('Setted up')
+    bot.infinity_polling()
--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,9 @@
 import sqlite3 as sl
 import hashlib
+import datetime
 
 def get_connection():
-    return sl.connect(r'C:\Users\rusty\Desktop\Programming\Python projects\flexbank\database\database.db')
+    return sl.connect(r'C:\Users\rusty\Desktop\Programming\Python projects\flexbank\database\database.db', check_same_thread=False)
 
 def check_authentication(conn, login, passw):
     cursor = conn.cursor()
@@ -28,6 +29,7 @@ def get_accounts_by_user(conn, id_user):
     result = cursor.fetchall()
     return [{
         'id':account[0],
+        'id_user':account[1],
         'type':account[2],
         'status':account[3],
         'number':account[4]
@@ -39,6 +41,7 @@ def get_active_accounts_by_user(conn, id_user):
     result = cursor.fetchall()
     return [{
         'id':account[0],
+        'id_user':account[1],
         'type':account[2],
         'status':account[3],
         'number':account[4]
@@ -50,8 +53,9 @@ def get_cards_by_account(conn, id_account):
     result = cursor.fetchall()
     return [{
         'id':card[0],
+        'id_account':card[1],
         'status':card[2],
-        'card_numbe':card[3],
+        'card_number':card[3],
         'validity_period':card[4],
         'cvc':card[5]
     } for card in result]
@@ -62,57 +66,106 @@ def get_active_cards_by_account(conn, id_account):
     result = cursor.fetchall()
     return [{
         'id':card[0],
+        'id_account':card[1],
         'status':card[2],
-        'card_numbe':card[3],
+        'card_number':card[3],
         'validity_period':card[4],
         'cvc':card[5]
     } for card in result]
 
+def get_account_by_id(conn, id_account):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM ACCOUNT WHERE id=?", (id_account, ))
+    account = cursor.fetchall()[0]
+    return {
+        'id':account[0],
+        'id_user':account[1],
+        'type':account[2],
+        'status':account[3],
+        'number':account[4]
+    }
+
+def get_card_by_id(conn, id_card):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM CARDS WHERE id=?", (id_card, ))
+    card = cursor.fetchall()[0]
+    return {
+        'id':card[0],
+        'id_account':card[1],
+        'status':card[2],
+        'card_number':card[3],
+        'validity_period':card[4],
+        'cvc':card[5]
+    }
+
 def block_account(conn, id_account):
     cursor = conn.cursor()
     cursor.execute("UPDATE ACCOUNT SET status='blocked' WHERE id=? AND status='active'", (id_account, ))
-    cursor.commit()
+    conn.commit()
 
 def activate_account(conn, id_account):
     cursor = conn.cursor()
     cursor.execute("UPDATE ACCOUNT SET status='active' WHERE id=? AND status='blocked'", (id_account, ))
-    cursor.commit()
+    conn.commit()
 
 def close_account(conn, id_account):
     cursor = conn.cursor()
     cursor.execute("UPDATE ACCOUNT SET status='closed' WHERE id=? AND status='active'", (id_account, ))
-    cursor.commit()
+    conn.commit()
 
 def block_card(conn, id_card):
     cursor = conn.cursor()
     cursor.execute("UPDATE CARDS SET status='blocked' WHERE id=? AND status='active'", (id_card, ))
-    cursor.commit()
+    conn.commit()
 
 def activate_card(conn, id_card):
     cursor = conn.cursor()
     cursor.execute("UPDATE CARDS SET status='active' WHERE id=? AND status='blocked'", (id_card, ))
-    cursor.commit()
+    conn.commit()
 
 def close_card(conn, id_card):
     cursor = conn.cursor()
-    cursor.execute("UPDATE CARDST SET status='closed' WHERE id=? AND status='active'", (id_card, ))
-    cursor.commit()
+    cursor.execute("UPDATE CARDS SET status='closed' WHERE id=? AND status='active'", (id_card, ))
+    conn.commit()
 
-def make_transaction():
+def get_transactions_by_user(conn, id_user):
+    accounts = get_accounts_by_user(conn, id_user)
+    accounts_id = ['id_account='+str(x['id']) for x in accounts]
     cursor = conn.cursor()
-    cursor.execute("UINSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');", (id_card, ))
+    cursor.execute("SELECT * FROM TRANSACTIONS WHERE "+' OR '.join(accounts_id))
+    result = cursor.fetchall()
+    return [{
+        'id':news[0],
+        'id_account':news[1],
+        'type':news[2],
+        'date':news[3],
+        'amount':news[4],
+        'another_subject':news[5]
+    } for news in result] 
+
+def get_transactions_by_account(conn, id_account):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM ACCOUNT WHERE id_account=?", (id_account, ))
+    result = cursor.fetchall()
+    return [{
+        'id':news[0],
+        'id_account':news[1],
+        'type':news[2],
+        'date':news[3],
+        'amount':news[4],
+        'another_subject':news[5]
+    } for news in result] 
+
+def make_transaction(conn, id_account, type, date, amount, another_subject):
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Customers (id_account, type, date, amount, another_subject) VALUES (?, ?, ?, ?, ?);", (id_account, type, date, amount, another_subject))
     cursor.commit()
 
-def transfer_from_to_card(conn, from_card, to_card, amount):
-    
+def make_between_our_cards(conn, id_from, id_to, amount):
+    card_from = get_card_by_id(conn, id_from)
+    card_to = get_card_by_id(conn, id_to)
+    make_transaction(conn, card_from['id_account'], card_to['id_account'], datetime.today().strftime("%d/%m/%y"), -amount)
+    make_transaction(conn, card_to['id_account'], card_from['id_account'], datetime.today().strftime("%d/%m/%y"), amount)
 
-login = 'WQLXCYMEUZ'
-passw = 'BGdcN1G72e'
-
-conn = get_connection()
-cursor = conn.cursor()
-#hashlib.sha256(x.encode('utf-8')).hexdigest()
-if check_authentication(conn, login, passw):
-    user = get_user_by_login_pass(conn, login, passw)
-    print(get_cards_by_account(conn, 1))
-conn.close()
+#login = 'WQLXCYMEUZ'
+#passw = 'BGdcN1G72e'
