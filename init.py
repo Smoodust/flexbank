c2433371f6c3c2cf4d5322b421dc5085a5265a03
Starwarik <Starwarik@bk.ru>
2022-11-24 14:33:31 +0400

    modified init.py

--- init.py
+++ init.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    print('hello!')
+import telebot
+
+bot = telebot.TeleBot('5815112728:AAHcJ_GcAXDAzemA731QYrKNY_w-leAWugI')
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.infinity_polling()
