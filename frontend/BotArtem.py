23421aed14b6dc7302b8dde1240da74ffc4e0ac3
Starwarik <Starwarik@bk.ru>
2022-11-24 20:30:14 +0300

    nothing 2

--- frontend/BotArtem.py
+++ frontend/BotArtem.py
@@ -1,13 +1,40 @@
 import asyncio
 import telebot as tb
-from telebot import types
 import kb
 
 bot = tb.TeleBot("5815112728:AAHcJ_GcAXDAzemA731QYrKNY_w-leAWugI")
 
 @bot.message_handler(commands=['start'])
-def Start(message):
+def start_menu(message):
     bot.send_message(message.chat.id, message.text, reply_markup=kb.main_menu)
+
+@bot.message_handler(commands=['dhisjhf'])
+def start(message):
+    message.text = 'Здравствуйте! Я банкинг-бот Neo! Для начала работы пройдите авторизацию.'
+    bot.send_message(message.chat.id, message.text)
+    message.text = 'Введите ваш *login и пароль.*'
+    bot.send_message(message.chat.id, message.text, parse_mode="Markdown")
+
+    # Здесь запрос к базе
+    message.text = 'Вас зовут ' + '?'
+    bot.send_message(message.chat.id, message.text)
+
+@bot.message_handler(chat_types=['text'])
+def bills(message):
+    if message.text == 'Счета':
+        bot.send_message(message.chat.id, message.text, reply_markup=kb.bills_menu)
+
+@bot.message_handler(chat_types=['text'])
+def back_to_menu(message):
+    if message.text == 'Назад в меню':
+        bot.send_message(message.chat.id, message.text, reply_markup=kb.main_menu)
+
+
+@bot.message_handler(content_types=['text'])
+def clear_buttons(message):
+    bot.send_message(message.chat.id, message.text, reply_markup=kb.empty)
+
+
 
 if __name__ == '__main__':
     bot.infinity_polling()
--- frontend/kb.py
+++ frontend/kb.py
@@ -1,8 +1,15 @@
 from telebot import types
 
 # Main menu buttons
-Bills = types.KeyboardButton('Счета')
-Operations = types.KeyboardButton('Операции')
-News = types.KeyboardButton('Новости')
+bills = types.KeyboardButton('Счета')
+operations = types.KeyboardButton('Операции')
+suggests = types.KeyboardButton('Предложения')
+news = types.KeyboardButton('Новости')
 
-main_menu = types.ReplyKeyboardMarkup(row_width=4).add(Bills, Operations, News) # Menu object
+main_menu = types.ReplyKeyboardMarkup(row_width=4).add(bills, operations, suggests, news) # Menu object
+
+
+
+back_tomenu = types.KeyboardButton('Назад в меню')
+empty = types.ReplyKeyboardRemove()
+bills_menu = types.ReplyKeyboardMarkup(row_width=1).add(back_tomenu)
